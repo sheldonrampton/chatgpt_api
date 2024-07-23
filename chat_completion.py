c303f517808aa0dd7c5992de20b85b146b1e653f
sheldonrampton <sheldonmrampton@gmail.com>
2024-07-22 22:05:33 -0500

    add json and vision

---
+++ chat_completion.py
@@ -0,0 +1,20 @@
+# https://platform.openai.com/docs/guides/chat-completions
+from openai import OpenAI
+
+client = OpenAI(
+  organization='org-M7JuSsksoyQIdQOGaTgA2wkk',
+  project='proj_E0H6uUDUEkSZfn0jdmqy206G',
+)
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
+
+# print(response.choices[0].message)
+print(response.choices[0].message.content)
---
+++ vision.py
@@ -0,0 +1,25 @@
+# https://platform.openai.com/docs/guides/vision
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {
+      "role": "user",
+      "content": [
+        {"type": "text", "text": "What’s in this image?"},
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": "https://static.wixstatic.com/media/0c0801_42f4fec97e9a4b0783e2b1caabe3e3f5~mv2.png/v1/crop/x_0,y_174,w_2688,h_1259/fill/w_1880,h_880,al_c,q_90,usm_0.66_1.00_0.01,enc_auto/youth_tennis.png",
+          },
+        },
+      ],
+    }
+  ],
+  max_tokens=300,
+)
+
+print(response.choices[0])
